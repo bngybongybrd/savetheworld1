8278a8ac96a05d1618ac66e2d581f0469327097c
yx <48951196+bngybongybrd@users.noreply.github.com>
2023-03-07 03:41:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+  {
+    'user': 'John',
+    'title': 'Post 1',
+    'content': 'Help',
+    'date': 'March 3, 2023'
+  },
+  {
+    'user': 'Tim',
+    'title': 'Post 2',
+    'content': 'Help',
+    'date': 'March 4, 2023'
+  }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+app.run(host='0.0.0.0', port=81)
